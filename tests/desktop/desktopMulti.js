4eff8c672d39abbfecc8b8be8f997695a16bef40
kurapi <279914410@qq.com>
2021-11-02 14:10:15 +0800

    Update desktopMulti.js

--- tests/desktop/desktopMulti.js
+++ tests/desktop/desktopMulti.js
@@ -3,19 +3,5 @@ module.exports = async function(context, commands) {
   commands.meta.setDescription(
     'First hit the Main_Page with an empty browser cache and then visit Barack, followed buy the Democratic Party'
   );
-  await commands.measure.start('https://en.wikipedia.org/wiki/Main_Page');
-  // Fake some read time
-  await commands.wait.byTime(21000);
-  await commands.js.run(
-    'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";'
-  );
-  await commands.measure.start('https://en.wikipedia.org/wiki/Barack_Obama');
-  // Fake some read time
-  await commands.wait.byTime(21000);
-  await commands.js.run(
-    'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";'
-  );
-  return commands.measure.start(
-    'https://en.wikipedia.org/wiki/Democratic_Party_(United_States)'
-  );
+  await commands.measure.start('https://new.hnair.com/hainanair/ibe/common/flightSearch.do');
 };
